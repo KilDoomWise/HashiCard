69104ed4ffa3ffb6adf93c42193dfa6ca12ffcb4
KilDoomWise <hinjamedia@gmail.com>
2024-01-03 00:39:37 +0300

    fix gui libary loading

--- modules/module_controller.lua
+++ modules/module_controller.lua
@@ -1,6 +1,8 @@
-local GUI = require("libaries/GUI")
+local GUI = require("GUI")
 
 print("Загружен модуль контроллера")
+print("Loading libary: GUI")
+os.execute("wget -f https://raw.githubusercontent.com/KilDoomWise/HashiCard/master/libaries/GUI.lua")
 print("Booting up GUI...")
 os.sleep(1)
 
@@ -12,4 +14,4 @@ application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D
 -- Add a rounded button
 application:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Rounded button")).onTouch = function()
 	GUI.alert("Rounded button was pressed")
-end
+end 
