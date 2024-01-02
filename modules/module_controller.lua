46ded7d470533d054f5ebb6dffdf55f262caa141
KilDoomWise <hinjamedia@gmail.com>
2024-01-03 00:36:48 +0300

    BTW working gui?

--- modules/module_controller.lua
+++ modules/module_controller.lua
@@ -1,1 +1,15 @@
+local GUI = require("libaries/GUI")
+
 print("Загружен модуль контроллера")
+print("Booting up GUI...")
+os.sleep(1)
+
+
+local application = GUI.application()
+
+application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D2D2D))
+
+-- Add a rounded button
+application:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Rounded button")).onTouch = function()
+	GUI.alert("Rounded button was pressed")
+end
